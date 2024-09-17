b424dcc5d8aec6af1fae621296a3eb1482961a04
Ashik <engineerashikur@gmail.com>
2024-09-17 17:27:31 +0600

    Added all the slides total price calculations

--- summer.js
+++ summer.js
@@ -41,3 +41,66 @@ document.getElementById('btn-one').addEventListener('click', function one(){
     const totalPrice = setTotal('total-price',totalValue);
 })
 
+// Two slide
+document.getElementById('btn-two').addEventListener('click', function two(){
+    const buttonOneInput = getText('para-two');
+    const totalPriceInput = getText('total-price');
+    const totalValue = ( buttonOneInput + totalPriceInput );
+    const totalPrice = setTotal('total-price',totalValue);
+})
+
+// Three slide
+document.getElementById('btn-three').addEventListener('click', function three(){
+    const buttonOneInput = getText('para-three');
+    const totalPriceInput = getText('total-price');
+    const totalValue = ( buttonOneInput + totalPriceInput );
+    const totalPrice = setTotal('total-price',totalValue);
+})
+
+// Four slide
+document.getElementById('btn-four').addEventListener('click', function four(){
+    const buttonOneInput = getText('para-four');
+    const totalPriceInput = getText('total-price');
+    const totalValue = ( buttonOneInput + totalPriceInput );
+    const totalPrice = setTotal('total-price',totalValue);
+})
+
+// Five slide
+document.getElementById('btn-five').addEventListener('click', function five(){
+    const buttonOneInput = getText('para-five');
+    const totalPriceInput = getText('total-price');
+    const totalValue = ( buttonOneInput + totalPriceInput );
+    const totalPrice = setTotal('total-price',totalValue);
+})
+
+// Six slide
+document.getElementById('btn-six').addEventListener('click', function six(){
+    const buttonOneInput = getText('para-six');
+    const totalPriceInput = getText('total-price');
+    const totalValue = ( buttonOneInput + totalPriceInput );
+    const totalPrice = setTotal('total-price',totalValue);
+})
+
+// Seven slide
+document.getElementById('btn-seven').addEventListener('click', function seven(){
+    const buttonOneInput = getText('para-seven');
+    const totalPriceInput = getText('total-price');
+    const totalValue = ( buttonOneInput + totalPriceInput );
+    const totalPrice = setTotal('total-price',totalValue);
+})
+
+// Eight slide
+document.getElementById('btn-eight').addEventListener('click', function eight(){
+    const buttonOneInput = getText('para-eight');
+    const totalPriceInput = getText('total-price');
+    const totalValue = ( buttonOneInput + totalPriceInput );
+    const totalPrice = setTotal('total-price',totalValue);
+})
+
+// Nine slide
+document.getElementById('btn-nine').addEventListener('click', function nine(){
+    const buttonOneInput = getText('para-nine');
+    const totalPriceInput = getText('total-price');
+    const totalValue = ( buttonOneInput + totalPriceInput );
+    const totalPrice = setTotal('total-price',totalValue);
+})
